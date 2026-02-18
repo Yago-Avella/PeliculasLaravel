8f39dbf6b0ae08adc3c87a454ff2da13a9f18c3a
Yago-Avella <yagoserer6@gmail.com>
2026-02-18 10:17:30 +0100

    Modelo Pelicula Cambiado

--- app/Models/Pelicula.php
+++ app/Models/Pelicula.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pelicula extends Model
 {
-    //
+    protected $table = 'peliculas'; // Nombre exacto de la tabla
+    protected $fillable = [
+        'titulo',
+        'anyo',
+        'duracion',
+        'sinopsis',
+        'poster',
+        'media',
+        'tmdb_id',
+    ];
 }
