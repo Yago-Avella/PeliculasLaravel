707db86b07d56bc02fd14ff5003d13bd4fba4b70
Yago Serer <yagserave@alu.edu.gva.es>
2026-02-18 18:30:02 +0100

    Ya funciona añadir y eliminar pelis to bien pero falta lo de los generos :)

--- app/Models/Pelicula.php
+++ app/Models/Pelicula.php
@@ -4,6 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Pelicula extends Model{
-    //
+class Pelicula extends Model
+{
+    // Lista de atributos que se pueden asignar en masa
+    protected $fillable = [
+        'tmdb_id',
+        'titulo',
+        'anyo',
+        'duracion',
+        'sinopsis',
+        'poster',
+        'media',
+    ];
+
+    // si es necesario, puedes definir relaciones aquí, por ejemplo:
+    // public function users()
+    // {
+    //     return $this->belongsToMany(User::class);
+    // }
 }
