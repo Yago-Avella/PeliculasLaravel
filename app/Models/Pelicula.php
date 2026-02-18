5c3e0f687186ec3be773d84e4bb65e91cbefbb50
Yago Serer <yagserave@alu.edu.gva.es>
2026-02-18 10:24:15 +0100

    Añadido de controladores de TMDB y cambio de Peliculas

--- app/Models/Pelicula.php
+++ app/Models/Pelicula.php
@@ -4,7 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Pelicula extends Model
-{
-    //
+class Pelicula extends Model{
+    
+    protected $table = 'peliculas'; // Nombre exacto de la tabla
+    protected $fillable = [
+        'titulo',
+        'anyo',
+        'duracion',
+        'sinopsis',
+        'poster',
+        'media',
+        'tmdb_id',
+    ];
 }
